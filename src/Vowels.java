1faf9bf56916d33b020d07482cb90af59988a3ab
NeelChavan <neelchavan9@gmail.com>
2022-03-30 15:47:06 +0530

    updated vowels

--- src/Vowels.java
+++ src/Vowels.java
@@ -1,20 +1,28 @@
+import java.util.Scanner;
 
 public class Vowels {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		String str = sc.nextLine();
 		int vowl = 0;
-		int consont = 0;
-		String sentence = "aeiou";
-		char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-		System.out.println(vowels);
-		for (char vowel : vowels)
-			for (char letter : sentence.toLowerCase().toCharArray())
-				if (letter == vowel)
-					vowl++;
-		System.out.println("Number of vowels in the given sentence is " + vowl);
-		System.out.println("Number of consont in the given sentence is " + consont);
+		int conso = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				vowl++;
+			} else if (ch >= 'a' && ch <= 'z') {
+				conso++;
+			}
+		}
+
+		System.out.println(vowl);
+		System.out.println(conso);
+
 	}
 
 }
